02a2a0bed6047e0b40fe1460d724cd2c923a914e
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-07-12 09:35:26 +0200

    Init: Project stub

---
+++ src/main/kotlin/de/difuture/ekut/pht/station/StationApplication.kt
@@ -0,0 +1,11 @@
+package de.difuture.ekut.pht.station
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StationApplication
+
+fun main(args: Array<String>) {
+    runApplication<StationApplication>(*args)
+}

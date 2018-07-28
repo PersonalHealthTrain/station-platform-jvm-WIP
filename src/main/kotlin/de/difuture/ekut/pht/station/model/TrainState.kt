75801988cba56896d2f1ab9acbce4e9d49437ca4
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-07-28 16:32:55 +0200

    Fix: Reset most stuff in station application

--- src/main/kotlin/de/difuture/ekut/pht/station/model/TrainState.kt
+++
@@ -1,7 +0,0 @@
-package de.difuture.ekut.pht.station.model
-
-enum class TrainState {
-
-    REMOTE,
-    PROCESSED
-}
--- src/main/kotlin/de/difuture/ekut/pht/station/repository/TrainRepository.kt
+++
@@ -1,6 +0,0 @@
-package de.difuture.ekut.pht.station.repository
-
-import de.difuture.ekut.pht.station.domain.Train
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface TrainRepository : JpaRepository<Train, Long>
